db97efc91505a8e5e9ef6f01e0eddc22fa194f2d
Michael Tigue <mtigue@web4uonline.com>
2013-06-02 15:45:52 -0400

    17

---
+++ _vti_cnf/messages.php
@@ -0,0 +1,23 @@
+vti_encoding:SR|utf8-nl
+vti_author:SR|web4u\\mtigue
+vti_modifiedby:SR|web4u\\mtigue
+vti_timelastmodified:TR|02 Jun 2013 19:39:44 -0000
+vti_timecreated:TR|02 Jun 2013 19:22:08 -0000
+vti_title:SR|Resources
+vti_extenderversion:SR|12.0.0.0
+vti_backlinkinfo:VX|
+vti_nexttolasttimemodified:TW|02 Jun 2013 19:35:01 -0000
+vti_syncofs_68.178.246.6\:21/adorationapp:TW|02 Jun 2013 19:40:11 -0000
+vti_syncwith_68.178.246.6\:21/adorationapp:TX|02 Jun 2013 19:39:44 -0000
+vti_cacheddtm:TX|02 Jun 2013 19:39:44 -0000
+vti_filesize:IR|2574
+vti_cachedtitle:SR|Resources
+vti_cachedbodystyle:SR|<body style="background:none !Important;">
+vti_cachedlinkinfo:VX|Q|http://code.jquery.com/mobile/1.3.1/jquery.mobile-1.3.1.min.css S|http://code.jquery.com/jquery-1.9.1.min.js S|http://code.jquery.com/mobile/1.3.1/jquery.mobile-1.3.1.min.js Q|css/web4u.css S|images/confession.png H|https://itunes.apple.com/us/app/confession-app-catholic-sacrament/id601823513 S|images/itunes.png H|https://play.google.com/store/apps/details S|images/play.png H|http://www.amazon.com/gp/product/B00BHMG6O4/ref=as_li_ss_tl S|images/amazon.png
+vti_cachedsvcrellinks:VX|NQHS|http://code.jquery.com/mobile/1.3.1/jquery.mobile-1.3.1.min.css NSHS|http://code.jquery.com/jquery-1.9.1.min.js NSHS|http://code.jquery.com/mobile/1.3.1/jquery.mobile-1.3.1.min.js FQUS|15/css/web4u.css FSUS|15/images/confession.png NHSS|https://itunes.apple.com/us/app/confession-app-catholic-sacrament/id601823513 FSUS|15/images/itunes.png NHSS|https://play.google.com/store/apps/details FSUS|15/images/play.png NHHS|http://www.amazon.com/gp/product/B00BHMG6O4/ref=as_li_ss_tl FSUS|15/images/amazon.png
+vti_cachedneedsrewrite:BR|false
+vti_cachedhasbots:BR|false
+vti_cachedhastheme:BR|false
+vti_cachedhasborder:BR|false
+vti_metatags:VR|HTTP-EQUIV=Content-Type text/html;\\ charset=utf-8 HTTP-EQUIV=refresh 500 viewport width=device-width,\\ initial-scale=1
+vti_charset:SR|utf-8
---
+++ messages.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+<meta content="text/html; charset=utf-8" http-equiv="Content-Type" />
+
+<meta http-equiv="refresh" content="500" url="http://www.web4uonline.com/adorationapp/15/resources.php"/> 
+
+	<meta name="viewport" content="width=device-width, initial-scale=1"/>
+	<link rel="stylesheet" href="http://code.jquery.com/mobile/1.3.1/jquery.mobile-1.3.1.min.css" />
+	<script src="http://code.jquery.com/jquery-1.9.1.min.js"></script>
+	<script src="http://code.jquery.com/mobile/1.3.1/jquery.mobile-1.3.1.min.js"></script>
+	
+	
+<title>Resources</title>
+
+<!----css----->
+	
+	<link rel="stylesheet" href="css/web4u.css" />
+
+	
+	
+<script>
+
+function close()
+{
+window.plugins.childBrowser.close();
+}
+
+function external()
+{
+window.plugins.childBrowser.openExternal('http://www.web4uonline.com');
+}
+
+
+</script>
+	
+
+</head>
+<body style="background:none !Important;">
+
+<div data-role="page">
+
+	
+	
+	
+<div data-role="content">	
+
+
+
+	
+
+
+<div class="box">
+
+<img src="images/confession.png">
+
+
+
+
+
+<h2>Now Available<br />
+Confession App: Catholic </h2>
+<p>
+The Confession App prepares you in a gentle and comforting way for the Catholic sacrament of Confession (Reconciliation). Place a check mark next to items you'd like to confess and they appear in the Confession Guide section for use during Confession. 
+Written with new penititant in mind, the tone and wording in the app is full of 
+grace and forgiveness. However, the app is not lightweight. It covers all of the 
+major and tough questions to help keep even the most experienced user engaged. 
+App data is never stored permanently to the phone.</p>
+	<p>
+	<strong>Available at:</strong></p>
+	
+	<div class="stores">	
+		<a href="https://itunes.apple.com/us/app/confession-app-catholic-sacrament/id601823513?mt=8" target="">
+		<img src="images/itunes.png" style="border:0px;"/></a>
+		
+		
+		<a href="https://play.google.com/store/apps/details?id=com.web4uonline.confession" target="">
+		<img src="images/play.png" style="border:0px;"/>
+		</a>
+	
+		
+		<a target="" href="http://www.amazon.com/gp/product/B00BHMG6O4/ref=as_li_ss_tl?ie=UTF8&camp=1789&creative=390957&creativeASIN=B00BHMG6O4&linkCode=as2&tag=cantici-20"><img src="images/amazon.png" style="border:0px;"/></a>
+
+
+</div>
+
+
+</div><!--end box--->
+
+
+</div>
+<!-----end content--->
+
+</div><!------end page----->
+
+
+</body>
+
+</html>
